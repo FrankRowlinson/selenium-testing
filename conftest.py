a91d28ec6c090e4886e9a8907e9b5c97ad5408d5
Vladislav Smolov <johk2293@gmail.com>
2022-07-10 14:05:26 +0400

    add conftest with browser fixture

---
+++ conftest.py
@@ -0,0 +1,13 @@
+# configuration file for all tests
+# can contain frequently used fixtures
+
+import pytest
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+
+@pytest.fixture(scope="function")
+def browser():
+    browser = webdriver.Chrome()
+    yield browser
+    browser.quit()

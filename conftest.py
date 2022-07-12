e95320e4ae7234fe5b75ab55d1968f9827c8d7e5
Vladislav Smolov <johk2293@gmail.com>
2022-07-12 10:47:42 +0400

    update config

--- conftest.py
+++ conftest.py
@@ -1,13 +1,32 @@
 # configuration file for all tests
 # can contain frequently used fixtures
-
 import pytest
 from selenium import webdriver
-from selenium.webdriver.common.by import By
+
+
+supported_browsers = {
+    'chrome': webdriver.Chrome,
+    'firefox': webdriver.Firefox
+}
+
+# now you can specify browser name in command line options by using --browser_name='browser-name'.
+# supported variants: chrome, firefox
+def pytest_addoption(parser):
+    parser.addoption('--browser_name', action='store', default='chrome',
+                     help="Choose browser: chrome or firefox")
 
 
 @pytest.fixture(scope="function")
-def browser():
-    browser = webdriver.Chrome()
+def browser(request):
+    browser_name = request.config.getoption("browser_name")
+
+    if browser_name in supported_browsers:
+        browser = supported_browsers.get(browser_name)()
+        print(f"\nstart {browser_name} browser for test..")
+    else:
+        joined_browsers = ', '.join(supported_browsers.keys())
+        raise pytest.UsageError(f"--browser_name is invalid, supported browsers: {joined_browsers}")
+
     yield browser
+    print("\nquit browser..")
     browser.quit()
